cc2c0f8efc1bf75ca44bcbe92f81db70d0343270
David Waters <davidwaters@google.com>
2011-11-15 01:46:12 +0000

    Add start of AdSenseClient

---
+++ Google.Adsense.Win.Logic/AdSenseApi/AdSenseClient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Google.Apis.Adsense.v1;
+using Google.Apis.Adsense.v1.Data;
+using Google.Apis.Util;
+
+namespace Google.Adsense.Win.Logic.AdSenseApi
+{
+    class AdSenseClient : IAdSenseClient
+    {
+        private readonly AdsenseService service;
+
+        public AdSenseClient(AdsenseService service)
+        {
+            service.ThrowIfNull("service");
+        }
+
+        public IList<string> FetchAdClients()
+        {
+            AdClients adclients = service.Adclients.List().Fetch();
+            return (from client in adclients.Items
+                    where client.SupportsReporting.HasValue && client.SupportsReporting.Value
+                    select client.Id).ToList();  
+        }
+
+        public OverviewReport FetchOverview()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ChannelSummary FetchCustomChannels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ChannelSummary FetchUrlChannels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AggregateRevenueSummary FetchYtdRevenue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AggregateRevenueSummary FetchLifetimeRevenue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Google.Adsense.Win.Logic/AdSenseApi/IAdSenseClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Google.Adsense.Win.Logic.AdSenseApi
+{
+    interface IAdSenseClient
+    {
+      IList<string> FetchAdClients();
+
+      OverviewReport FetchOverview();
+  
+      ChannelSummary FetchCustomChannels();
+  
+      ChannelSummary FetchUrlChannels();
+  
+      AggregateRevenueSummary FetchYtdRevenue();
+
+      AggregateRevenueSummary FetchLifetimeRevenue();
+    }
+
+    class OverviewReport { }
+    class ChannelSummary { }
+    class AggregateRevenueSummary { }
+}
